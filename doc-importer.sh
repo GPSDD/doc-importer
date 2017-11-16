ebb8fa51de04d9d82a7896c1bc06fdff7f1ff36b
rrequero <rrequero@gmail.com>
2017-11-16 11:30:15 +0100

    update

--- doc-importer.sh
+++ doc-importer.sh
@@ -4,23 +4,19 @@ case "$1" in
     pull-sub)
         (cd executor && git pull origin $2)
         (cd orchestrator && git pull origin $2)
-        (cd writter && git pull origin $2)
+        (cd writer && git pull origin $2)
         ;;
     co-sub)
         (cd executor && git checkout $2)
         (cd orchestrator && git checkout $2)
-        (cd writter && git checkout $2)
+        (cd writer && git checkout $2)
         ;;
 
     develop)
         type docker-compose >/dev/null 2>&1 || { echo >&2 "docker-compose is required but it's not installed.  Aborting."; exit 1; }
         docker-compose -f docker-compose-develop.yml build && docker-compose -f docker-compose-develop.yml up
         ;;
-    test)
-        type docker-compose >/dev/null 2>&1 || { echo >&2 "docker-compose is required but it's not installed.  Aborting."; exit 1; }
-        docker-compose -f docker-compose-test.yml build && docker-compose -f docker-compose-test.yml up
-        ;;
-  *)
+    *)
         echo "Usage: doc-importer.sh {test-e2e|test-unit|start|develop|test}" >&2
         exit 1
         ;;
